d28927247e4b83b98cc7cbdb9440b19aa7f04cdf
kanitsharma <kanitshrm09@gmail.com>
2019-01-19 09:06:48 +0530

    Updated Basic example

--- example/basic/index.js
+++ example/basic/index.js
@@ -1,46 +1,35 @@
 import run from '../../dist/main';
-import { f, onClick, onEnter, value, Text, h } from "../../dist/main";
-import { Union, Record, Effect } from '../../dist/main'
+import { f, onClick, Text } from "../../dist/main";
+import { Union, Record } from '../../dist/main'
 
 const Model = Record({
-  counter: Number,
-  text: String
+  counter: Number
 })
 
 // init :: Model
-const Init = Model.create(0, 'Hello')
+const Init = Model.create(0)
 
+// Msg = Increment | Decrement
 const Msg = Union({
   Increment: [],
-  Decrement: [],
-  UpdateText: [String]
+  Decrement: []
 });
 
 // update :: Model -> Msg -> Model
 const Update = model => Msg.case({
   Increment: () => ({ ...model, counter: model.counter + 1 }),
   Decrement: () => ({ ...model, counter: model.counter - 1 }),
-  UpdateText: text => ({ ...model, text }),
   _: () => model
-}) // msg will be partially applied here
+})
 
 // view :: Model -> Html Msg
 const View = ({ counter, text }) =>
-  f(
-    "div",
-    [],
-    [
-      f("button", [onClick(Msg.Increment)], ["+"]),
-      f("span", [], [Text(counter)]),
-      f("button", [onClick(Msg.Decrement)], ["-"]),
-      TextBox(text, Msg.UpdateText),
-      f("h1", [], [Text(text)])
-    ]
+  f("div", [], [
+    f("button", [onClick(Msg.Increment)], ["+"]),
+    f("span", [], [Text(counter)]),
+    f("button", [onClick(Msg.Decrement)], ["-"]),
+  ]
   );
-
-const TextBox = (text, updateText) => (
-  f("input", [onEnter(e => updateText(e.target.value)), value(text)], [])
-)
 
 // Run
 const Root = document.getElementById("root");
