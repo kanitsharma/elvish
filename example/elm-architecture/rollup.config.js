74cae37a4a19a5efe3e2fdfac253b66a6f0810bb
kanitsharma <kanitshrm09@gmail.com>
2019-01-29 10:58:49 +0530

    Updated elm-architecture hyperscript to dedicated functions

--- example/elm-architecture/rollup.config.js
+++ example/elm-architecture/rollup.config.js
@@ -1,15 +1,15 @@
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-import commonJS from 'rollup-plugin-commonjs'
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonJS from "rollup-plugin-commonjs";
 import { uglify } from "rollup-plugin-uglify";
-import replace from 'rollup-plugin-replace'
+import replace from "rollup-plugin-replace";
 
 export default {
-  input: 'example/elm-architecture/index.js',
+  input: "example/elm-architecture/index.js",
   output: {
-    name: 'main',
-    file: 'example/elm-architecture/dist/main.js',
-    format: 'iife'
+    name: "main",
+    file: "example/elm-architecture/dist/main.js",
+    format: "iife"
   },
   plugins: [
     resolve({
@@ -17,13 +17,13 @@ export default {
       browser: true
     }),
     commonJS({
-      include: 'node_modules/**'
+      include: "node_modules/**"
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify( 'production' )
+      "process.env.NODE_ENV": JSON.stringify("production")
     }),
     babel({
-      exclude: 'node_modules/**'
+      exclude: "node_modules/**"
     }),
     uglify()
   ]
