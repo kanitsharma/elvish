0f26627fb5133a1cebd7005203faa54f34306ed6
kanitsharma <kanitshrm09@gmail.com>
2019-01-29 11:08:33 +0530

    Curried vdom elements

--- src/dom-effect/vdom.js
+++ src/dom-effect/vdom.js
@@ -1,4 +1,6 @@
 import { h } from 'inferno-hyperscript'
+import curryN from 'ramda/es/curryN'
+
 const reduceProps = props => props.reduce((acc, x) => ({ ...acc, ...x }), {});
 
 const x = (x, y, ...z) => h(x, y, z)
@@ -10,21 +12,21 @@ export const f = (type, propList, children) => h(
   children.filter(x => x)
 );
 
-export const div = (propList, children) => h('div', reduceProps(propList), children)
-export const button = (propList, children) => h('button', reduceProps(propList), children)
-export const input = (propList, children) => h('input', reduceProps(propList), children)
-export const p = (propList, children) => h('p', reduceProps(propList), children)
-export const h1 = (propList, children) => h('h1', reduceProps(propList), children)
-export const h2 = (propList, children) => h('h2', reduceProps(propList), children)
-export const h3 = (propList, children) => h('h3', reduceProps(propList), children)
-export const h4 = (propList, children) => h('h4', reduceProps(propList), children)
-export const h5 = (propList, children) => h('h5', reduceProps(propList), children)
-export const select = (propList, children) => h('select', reduceProps(propList), children)
-export const textarea = (propList, children) => h('textarea', reduceProps(propList), children)
-export const br = (propList, children) => h('br', reduceProps(propList), children)
-export const hr = (propList, children) => h('hr', reduceProps(propList), children)
-export const ul = (propList, children) => h('ul', reduceProps(propList), children)
-export const ol = (propList, children) => h('ol', reduceProps(propList), children)
-export const li = (propList, children) => h('li', reduceProps(propList), children)
-export const span = (propList, children) => h('span', reduceProps(propList), children)
+export const div = curryN(2, (propList, children) => h('div', reduceProps(propList), children))
+export const button = curryN(2, (propList, children) => h('button', reduceProps(propList), children))
+export const input = curryN(2, (propList, children) => h('input', reduceProps(propList), children))
+export const p = curryN(2, (propList, children) => h('p', reduceProps(propList), children))
+export const h1 = curryN(2, (propList, children) => h('h1', reduceProps(propList), children))
+export const h2 = curryN(2, (propList, children) => h('h2', reduceProps(propList), children))
+export const h3 = curryN(2, (propList, children) => h('h3', reduceProps(propList), children))
+export const h4 = curryN(2, (propList, children) => h('h4', reduceProps(propList), children))
+export const h5 = curryN(2, (propList, children) => h('h5', reduceProps(propList), children))
+export const select = curryN(2, (propList, children) => h('select', reduceProps(propList), children))
+export const textarea = curryN(2, (propList, children) => h('textarea', reduceProps(propList), children))
+export const br = curryN(2, (propList, children) => h('br', reduceProps(propList), children))
+export const hr = curryN(2, (propList, children) => h('hr', reduceProps(propList), children))
+export const ul = curryN(2, (propList, children) => h('ul', reduceProps(propList), children))
+export const ol = curryN(2, (propList, children) => h('ol', reduceProps(propList), children))
+export const li = curryN(2, (propList, children) => h('li', reduceProps(propList), children))
+export const span = curryN(2, (propList, children) => h('span', reduceProps(propList), children))
 
