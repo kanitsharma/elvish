188e09a46776c99dcef583abae9805b4e3be78fb
kanitsharma <kanitshrm09@gmail.com>
2019-03-26 17:42:51 +0530

    Added @kimera/io as Effect

--- src/core/types.js
+++ src/core/types.js
@@ -1,5 +1,6 @@
 import { Type } from "./union";
 import curryN from "ramda/es/curryN";
+export { default as Effect } from '@kimera/io';
 
 // Union
 export { Type as Union };
@@ -22,38 +23,3 @@ export const Record = rec => {
     ...X
   };
 };
-
-// Effect
-
-const empty = _ => Effect(_ => {});
-
-export const Effect = (F, cleanup = () => {}) => {
-  const map = f =>
-    Effect((reject, resolve) => F(x => reject(x), y => resolve(f(y))), cleanup);
-  const chain = f =>
-    Effect(
-      (reject, resolve) => F(x => reject(x), y => f(y).fork(reject, resolve)),
-      cleanup
-    );
-  const orElse = f =>
-    Effect(
-      (reject, resolve) => F(x => f(x).fork(reject, resolve), y => resolve(y)),
-      cleanup
-    );
-  const fold = (f, g) =>
-    Effect((_, resolve) => F(x => resolve(f(x)), y => resolve(g(y))), cleanup);
-  const cata = pattern => fold(pattern.Rejected, pattern.Resolved);
-  const bimap = (f, g) =>
-    Effect(
-      (reject, resolve) => F(x => reject(f(x)), y => resolve(g(y))),
-      cleanup
-    );
-  const fork = F;
-
-  return { map, chain, empty, orElse, fold, cata, bimap, fork };
-};
-
-Effect.of = x => Effect((_, resolve) => resolve(x));
-Effect.rejected = x => Effect(reject => reject(x));
-Effect.empty = empty;
-Effect.toString = () => "Effect";
